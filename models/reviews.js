1bbbfab7dd16b1877c12b26b02b8ee1b733ff97f
D0491 <dan.fenichel@yahoo.com>
2020-02-05 10:58:59 -0600

    added new Review model

---
+++ models/reviews.js
@@ -0,0 +1,33 @@
+module.exports = function(sequelize, DataTypes) {
+	const Review = sequelize.define("Review", {
+		address: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		rating: {
+			type: DataTypes.INTEGER,
+			allowNull: false
+		},
+		home_quality_review: {
+			type: DataTypes.STRING,
+			allowNull: true
+		},
+		user_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: "User",
+				key: "id"
+			}
+		}
+	});
+
+	Review.associate = function(models) {
+		Review.belongsTo(models.User, {
+			foreignKey: "id",
+			target_Key: "user_id"
+		});
+	};
+
+	return Review;
+};
--- models/user.js
+++ models/user.js
@@ -27,5 +27,13 @@ module.exports = function(sequelize, DataTypes) {
 	User.addHook("beforeCreate", function(user) {
 		user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
 	});
+
+	User.associate = function(models) {
+		User.hasMany(models.Review, {
+			foreignKey: "user_id",
+			onDelete: "cascade"
+		});
+	};
+
 	return User;
 };
