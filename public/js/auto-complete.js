d177ec0fcbe44ef3120bed15637230211044dab1
Kurt LaVacque <lavacquek@icloud.com>
2020-02-05 10:24:07 -0600

    Fixing Linting issue again

--- public/js/auto-complete.js
+++ public/js/auto-complete.js
@@ -1,23 +1,23 @@
 var placeSearch, autocomplete;
 var componentForm = {
-    street_number: 'short_name',
-    route: 'long_name',
-    locality: 'long_name',
-    administrative_area_level_1: 'short_name',
-    country: 'long_name',
-    postal_code: 'short_name'
+    street_number: "short_name",
+    route: "long_name",
+    locality: "long_name",
+    administrative_area_level_1: "short_name",
+    country: "long_name",
+    postal_code: "short_name"
 };
 
 function initAutocomplete() {
     // Create the autocomplete object, restricting the search to geographical
     // location types.
     autocomplete = new google.maps.places.Autocomplete(
-    /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
-        { types: ['geocode'] });
+    /** @type {!HTMLInputElement} */(document.getElementById("autocomplete")),
+        { types: ["geocode"] });
 
     // When the user selects an address from the dropdown, populate the address
     // fields in the form.
-    autocomplete.addListener('place_changed', fillInAddress);
+    autocomplete.addListener("place_changed", fillInAddress);
 }
 
 function fillInAddress() {
@@ -25,7 +25,7 @@ function fillInAddress() {
     var place = autocomplete.getPlace();
 
     for (var component in componentForm) {
-        document.getElementById(component).value = '';
+        document.getElementById(component).value = "";
         document.getElementById(component).disabled = false;
     }
 
--- public/js/index.js
+++ public/js/index.js
@@ -4,16 +4,16 @@ const search_form_f_e = $("#search_form_f");
 
 
 const get_search_result = () => {
-    const search_result = search_i_e.val()
+    const search_result = search_i_e.val();
     print(search_result);
-}
+};
 
 const print = x => console.log(x);
 
 search_form_f_e.on("submit", (event) => {
     event.preventDefault();
-    get_search_result()
-})
+    get_search_result();
+});
 
 
 
