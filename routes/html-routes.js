4fdccf75a138d97fcadd67b2bf162813691e5fe3
lil L <57925562+leolopez10@users.noreply.github.com>
2020-02-06 09:41:47 -0600

    pushing to pull again

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,19 +14,19 @@ const login_view = require("../public/views/login_view");
 const invite_friends_view = require("../public/views/invite_friends_view");
 // const monday_view = require("./views/monday_view");
 
-module.exports = function (app) {
+module.exports = function(app) {
 
-    app.get("/", function (req, res) {
+    app.get("/", function(req, res) {
         // If the user already has an account send them to the members pag
         // res.sendFile(path.join(__dirname, "../public/html/index.html"));
         if (req.user) {
             // res.redirect("/members");
-            return res.send(main_layout(home_view(),"members", "Profile"));
+            return res.send(main_layout(home_view(), "members", "Profile"));
         }
-        res.send(main_layout(home_view(),"login", "Login"));
+        res.send(main_layout(home_view(), "login", "Login"));
     });
 
-    app.get("/signup", function (req, res) {
+    app.get("/signup", function(req, res) {
         // If the user already has an account send them to the members page
         if (req.user) {
             res.redirect("/members");
@@ -35,40 +35,40 @@ module.exports = function (app) {
         res.send(main_layout(signup_view()));
     });
 
-    app.get("/map", function (req, res) {
+    app.get("/map", function(req, res) {
         // If the user already has an account send them to the members page
         // res.sendFile(path.join(__dirname, "../public/html/map.html"));
         if (req.user) {
             // res.redirect("/members");
-            return res.send(main_layout(map_view(),"members", "Profile"));
+            return res.send(main_layout(map_view(), "members", "Profile"));
         }
-        res.send(main_layout(map_view(),"login", "Login"));
+        res.send(main_layout(map_view(), "login", "Login"));
     });
 
-    app.get("/invite-friends", function (req, res) {
+    app.get("/invite-friends", function(req, res) {
         // If the user already has an account send them to the members page
         // res.sendFile(path.join(__dirname, "../public/html/invite_friends.html"));
         if (req.user) {
             // res.redirect("/members");
-            return res.send(main_layout(invite_friends_view(),"members", "Profile"));
+            return res.send(main_layout(invite_friends_view(), "members", "Profile"));
         }
-        res.send(main_layout(invite_friends_view(),"login", "Login"));
+        res.send(main_layout(invite_friends_view(), "login", "Login"));
     });
 
-    app.get("/login", function (req, res) {
+    app.get("/login", function(req, res) {
         // If the user already has an account send them to the members page
         if (req.user) {
             res.redirect("/members");
         }
         // res.sendFile(path.join(__dirname, "../public/html/login.html"));
-        res.send(main_layout(login_view(),"login", "Login"));
+        res.send(main_layout(login_view(), "login", "Login"));
     });
 
     // Here we've add our isAuthenticated middleware to this route.
     // If a user who is not logged in tries to access this route they will be redirected to the signup page
-    app.get("/members", isAuthenticated, function (req, res) {
+    app.get("/members", isAuthenticated, function(req, res) {
         // res.sendFile(path.join(__dirname, "../public/html/members.html"));
-        res.send(main_layout(profile_view(),"logout", "Logout"));
+        res.send(main_layout(profile_view(), "logout", "Logout"));
     });
 
 };
