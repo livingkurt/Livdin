97a96e623e427b7d1e58c11587c7aa95a9557281
Kurt LaVacque <lavacquek@icloud.com>
2020-02-06 11:25:11 -0600

    Fixed Linting Issue

--- public/js/map.js
+++ public/js/map.js
@@ -45,17 +45,16 @@
 // };
 
 window.onload = function() {
-
     //Initiate API key
-    L.mapquest.key = 'nkL6LFerG2cvr74dIKmAFOfVpGn5ACIZ';
+    L.mapquest.key = "nkL6LFerG2cvr74dIKmAFOfVpGn5ACIZ";
 
     //Define current location based of user input in the future
     var chosenLocation = {
-        street: '222 Gibson Lake Rd',
-        city: 'Crystal Falls',
-        state: 'MI',
-        postalCode: '49920'
-    }
+        street: "222 Gibson Lake Rd",
+        city: "Crystal Falls",
+        state: "MI",
+        postalCode: "49920"
+    };
 
     //Run function create map using the user location
     L.mapquest.geocoding().geocode(chosenLocation, createMap);
@@ -64,15 +63,15 @@ window.onload = function() {
     function createMap(error, response) {
         var location = response.results[0].locations[0];
         var latLng = location.displayLatLng;
-        var map = L.mapquest.map('map', {
+        var map = L.mapquest.map("map", {
             center: latLng,
-            layers: L.mapquest.tileLayer('map'),
+            layers: L.mapquest.tileLayer("map"),
             zoom: 18
         });
 
         //This creates a custom marker on the map
         var customIcon = L.mapquest.icons.circle({
-            primaryColor: '#3b5998'
+            primaryColor: "#3b5998"
         });
 
         //This attaches the marker to the map
@@ -81,4 +80,4 @@ window.onload = function() {
         }).addTo(map);
 
     }
-}
+};
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,7 +12,7 @@ const map_view = require("../public/views/map_view");
 const signup_view = require("../public/views/signup_view");
 const login_view = require("../public/views/login_view");
 const invite_friends_view = require("../public/views/invite_friends_view");
-const map_script_view = require("../public/views/map_script_view");
+// const map_script_view = require("../public/views/map_script_view");
 // const monday_view = require("./views/monday_view");
 
 module.exports = function (app) {
