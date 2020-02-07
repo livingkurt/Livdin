1e6d2dd732c9456a3d968da54eed240422598299
Kurt LaVacque <lavacquek@icloud.com>
2020-02-07 13:34:50 -0600

    Removed some comments and fixed an error that wasnt even there

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,4 @@
-// const parser = require("../../server");
 
-// var parsed = parser.parseLocation('1005 N Gravenstein Highway Sebastopol CA 95472');
-
-// const search_b_e = $("#search_b");
 const search_i_e = $("#search_i");
 const search_form_f_e = $("#search_form_f");
 
@@ -12,41 +8,10 @@ const get_search_result = () => {
     // search_result = search_result.replace(/[^\w\s]/gi, "");
     window.open(`/map/${search_result}`, "_self");
     
-    // search_result = search_result.replace(" ", "+");
-    // print(search_result);
-    
-    
-    
-    // print(params);
-    // window.open(`/map/${search_result}`, "_self");
-    
-    // const parsed = parser.parseLocation(search_result);
-    // print(parsed);
-    // window.open(`/map/${search_result}`);
-    // window.open("/map");
-    // var params = new URLSearchParams(window.location.search.slice(1));
-    // console.log(params.get())
 };
 
-// const print = x => console.log(x);
 
 search_form_f_e.on("submit", (event) => {
     event.preventDefault();
     get_search_result();
 });
-
-
-/////function to get the user input for the axios call/////
-// $("#search_b").on("click", function (event) {
-//     event.preventDefault();
-
-//     var homeSearch = {
-//         address: $("#search_i").val().trim()
-//     };
-//     console.log("index" + JSON.stringify(homeSearch));
-//     $.post("api/searched", homeSearch)
-//         .then(function (data) {
-//             console.log(data);
-//         });
-//     // location.href = "/map";
-// });
