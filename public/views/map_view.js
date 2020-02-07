54225823c7033210d3e0bf503a569292fcbf3493
Ryan Borja <rcborja.school@gmail.com>
2020-02-07 14:21:56 -0600

    got listings over map

--- public/views/map_view.js
+++ public/views/map_view.js
@@ -17,11 +17,11 @@
 //         </div>
 //         <div class="mapbox"id="map" style="width: 100%; height: 100vh;"></div>
 //     </div>
-    
+
 //     <footer id="footer">
 
 //     </footer>
-    
+
 //     <script src="../js/map.js"></script>
 // </body>
 
@@ -33,23 +33,26 @@
 
 
 
-module.exports = function(){
+module.exports = function () {
     return `
     <div>
         <div id="map_container">
-            <form class="overlay"id="search_form_f_map" autocomplete="on">
-                <input onfocus="this.placeholder = ''" onblur="this.placeholder='Address, Neighborhood, City, County'" type="text" name="" id="search_i_map" placeholder="Address, Neighborhood, City, County">
-                <button target="_self" id="search_b_map">Search</button>
-            </form>
-            <div class="overlay" id="search_results">
-                <p id="address_search"for="">Address:</p>
-                <p id="rating_search"for="">Price:</p>
-                <p id="review_search">Sqft: ft</p>
-                <p id="review_search">Beds/Bath: </p>
-                <img src=>
+            <div id="search_box">
+                <form class="overlay"id="search_form_f_map" autocomplete="on">
+                    <input onfocus="this.placeholder = ''" onblur="this.placeholder='Address, Neighborhood, City, County'" type="text" name="" id="search_i_map" placeholder="Address, Neighborhood, City, County">
+                    <button target="_self" id="search_b_map">Search</button>
+                </form>
+                <div class="overlay" id="search_results">
+                    <p id="address_search"for="">Address:</p>
+                    <p id="rating_search"for="">Price:</p>
+                    <p id="review_search">Sqft: ft</p>
+                    <p id="review_search">Beds/Bath: </p>
+                    <img src=>
+                </div>
             </div>
+            <div class="mapbox"id="map" style="width: 100%; height: 100vh;"></div>
         </div>
-        <div class="mapbox"id="map" style="width: 100%; height: 100vh;"></div>
+        
     </div>
     
     <footer id="footer">
