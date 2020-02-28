de878a6824c4da8240321d1bafcdda92c4d51660
Kurt LaVacque <lavacquek@icloud.com>
2020-02-27 21:27:57 -0600

    When the checkbox is pressed it will decide to show the checkbox on the map view or not

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,38 @@
 
 const search_i_e = $("#search_i");
 const search_form_f_e = $("#search_form_f");
+const checkbox = $("#checkbox")
+const checkbox_div = $("#slider_container_div_m")
+
+let on_off = '&';
+
+
+console.log(checkbox.val());
+
+$(document).ready(function () {
+  $('#checkbox').on('change', function () {
+    var isChecked = $(this).is(':checked');
+    if (isChecked) {
+      on_off = 'X'
+      checkbox_div.attr("style", "display: block");
+    } else {
+      on_off = '&'
+      checkbox_div.attr("style", "display: none");
+    }
+  });
+});
 
 const get_search_result = () => {
-    let search_result = search_i_e.val();
-    window.open(`/map/${search_result}`, "_self");
+  let search_result = search_i_e.val();
+  console.log(on_off)
+  window.open(`/map/${search_result}${on_off}`, "_self");
 };
 
 search_form_f_e.on("submit", (event) => {
-    event.preventDefault();
-    get_search_result();
+  event.preventDefault();
+  get_search_result();
 });
+
+
+
+
